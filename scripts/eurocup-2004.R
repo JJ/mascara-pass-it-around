2f8812dce171a4303760553eb69b49848a474e58
JJ Merelo <jjmerelo@gmail.com>
2022-10-24 18:31:32 +0200

    Adds template and start of paper, modifies code

--- scripts/eurocup-2004.R
+++ scripts/eurocup-2004.R
@@ -6,7 +6,12 @@ net_euro_2004 <- function( data_file ) {
   net.2004 <- graph_from_adjacency_matrix(adj_matrix,mode="undirected")
   isolated <- which(degree(net.2004)==0)
   net.2004 <- delete.vertices(net.2004, isolated)
-  plot(net.2004)
+  E(net.2004)$weight <- 1
+  net.2004.simplified <- simplify(net.2004,
+                                  edge.attr.comb = list(weight = "sum"))
+  plot(net.2004.simplified,edge.width = E(net.2004.simplified)$weight)
 }
 
-net_euro_2004("data/39-3.csv")
+for (i in c("data/39-3.csv","data/95-3.csv","data/110-3.csv","data/122-3.csv")) {
+  net_euro_2004(i)
+}
