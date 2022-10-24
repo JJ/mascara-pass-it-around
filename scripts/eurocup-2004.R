5f00c3db6eafb30c648d5b240fb14ce1ef8cb605
JJ Merelo <jjmerelo@gmail.com>
2022-10-24 18:09:03 +0200

    Starting to analyze euro cup 2004

--- scripts/eurocup-2004.R
+++ scripts/eurocup-2004.R
@@ -1,3 +1,12 @@
 library(igraph)
 
-portugal.2004 <- read.csv("data/110-3.csv",fileEncoding="latin1",row.names=1, check.names=FALSE, na.strings = "")
+
+net_euro_2004 <- function( data_file ) {
+  adj_matrix <- as.matrix(read.csv(data_file,fileEncoding="latin1",row.names=1, check.names=FALSE, na.strings = ""))
+  net.2004 <- graph_from_adjacency_matrix(adj_matrix,mode="undirected")
+  isolated <- which(degree(net.2004)==0)
+  net.2004 <- delete.vertices(net.2004, isolated)
+  plot(net.2004)
+}
+
+net_euro_2004("data/39-3.csv")
