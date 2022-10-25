e32f70fec02d5ea73582dfe36c352ebe41064f06
JJ Merelo <jjmerelo@gmail.com>
2022-10-25 21:02:08 +0200

    Improves biblio, saves eurocup

--- scripts/eurocup-2004-dl.R
+++ scripts/eurocup-2004-dl.R
@@ -2,7 +2,7 @@ library(igraph)
 
 
 net_euro_2004 <- function( data_file ) {
-  adj_matrix <- as.matrix(read.csv(data_file,fileEncoding="latin1",row.names=1, check.names=FALSE, na.strings = ""))
+  # adj_matrix <- read_graph( data_file, format="dl")
   net.2004 <- graph_from_adjacency_matrix(adj_matrix,mode="undirected")
   isolated <- which(degree(net.2004)==0)
   net.2004 <- delete.vertices(net.2004, isolated)
@@ -12,6 +12,6 @@ net_euro_2004 <- function( data_file ) {
   plot(net.2004.simplified,edge.width = E(net.2004.simplified)$weight)
 }
 
-for (i in c("data/39-3.csv","data/95-3.csv","data/110-3.csv","data/122-3.csv")) {
+for (i in c("data/alemania.dl")) {
   net_euro_2004(i)
 }
