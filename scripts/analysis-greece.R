46aa9c48f80883ddd6de9c1267f62dc6fcef23ce
JJ Merelo <jjmerelo@gmail.com>
2022-11-02 09:30:16 +0100

    Adds plotting

--- scripts/analysis-greece.R
+++ scripts/analysis-greece.R
@@ -16,19 +16,26 @@ net_euro_2004 <- function( data_file ) {
   return(net.2004.simplified)  
 }
 
-
 grecia <- data.frame(game=character(),entropy=numeric())
 grecia.nets <- list()
-bd.ranking <- data.frame(player = character(), game = character(), bd = numeric())
+bd.ranking <- data.frame(player = character(), team = character(), game = character(), bd = numeric(), entropy = numeric(), betweenness = numeric())
 for (i in c("grecia", "grecia-2", "grecia-3", "grecia-5", "grecia-6")) {
   this.net <- net_euro_2004(paste0("data/",i,".dl.csv"))
   grecia.nets[[i]] = this.net
   grecia <- rbind(grecia,data.frame(game=i,entropy=entropy(E(this.net)$weight)))
-  bd.ranking <- rbind(bd.ranking,data.frame(player=V(this.net)$name,game=rep(i,length(V(this.net)$name)),bd=V(this.net)$bd))
+  bd.ranking <- rbind(bd.ranking,
+                      data.frame(player=V(this.net)$name,
+                                 team="Greece",
+                                 game=rep(i,length(V(this.net)$name)),
+                                 bd=V(this.net)$bd,
+                                 entropy=V(this.net)$diversity,
+                                 betweenness=V(this.net)$betweenness)
+  )
 }
 
 portugal.1 <- net_euro_2004("data/portugal.dl.csv")
-bd.ranking <- rbind(bd.ranking,data.frame(player=V(portugal.1)$name,game=rep("portugal",length(V(portugal.1)$name)),bd=V(portugal.1)$bd))
+bd.ranking <- rbind(bd.ranking,data.frame(player=V(portugal.1)$name,game=rep("portugal",length(V(portugal.1)$name)),bd=V(portugal.1)$bd),entropy=V(portugal.1)$diversity,
+                    betweenness=V(portugal.1)$betweenness)
 portugal <- data.frame(game="portugal",entropy=entropy(E(portugal.1)$weight))
 portugal.nets <- list()
 portugal.nets[['portugal']] = portugal.1
@@ -37,7 +44,12 @@ for (i in 2:6) {
   game <- paste0("portugal-",i)
   portugal.nets[[game]] = this.net
   portugal <- rbind(portugal,data.frame(game=game,entropy=entropy(E(this.net)$weight)))
-  bd.ranking <- rbind(bd.ranking,data.frame(player=V(this.net)$name,game=rep(game,length(V(this.net)$name)),bd=V(this.net)$bd))
+  bd.ranking <- rbind(bd.ranking,
+                      data.frame(player=V(this.net)$name,
+                                 team="Portugal",
+                                 game=rep(game,length(V(this.net)$name)),bd=V(this.net)$bd,
+                                            entropy=V(this.net)$diversity,
+                                            betweenness=V(this.net)$betweenness))
 }
 
 
