791865f95c3ca6fad20036dd1b6acb5a4fcf8382
JJ Merelo <jjmerelo@gmail.com>
2022-11-05 13:42:03 +0100

    Edge entropies computed

--- scripts/analysis-greece.R
+++ scripts/analysis-greece.R
@@ -2,16 +2,29 @@ library(tidyverse)
 library(dplyr)
 library(igraph)
 library(entropy)
+source("scripts/net_euro_2004.R")
 
-source("net_euro_2004.R")
-
-grecia <- data.frame(game=character(),entropy=numeric())
+grecia <- data.frame(game=character(),entropy=numeric(),normEntropy=numeric())
 grecia.nets <- list()
+grecia.norm <- c()
 bd.ranking <- data.frame(player = character(), team = character(), game = character(), bd = numeric(), entropy = numeric(), betweenness = numeric())
+
+entropies <- function( a.vector ) {
+  states <- length(unique(a.vector))
+  entropy <- entropy(a.vector)
+  normEntropy <- entropy/log2(states)
+  return(c(entropy,normEntropy))
+}
+
 for (i in c("grecia", "grecia-2", "grecia-3", "grecia-5", "grecia-6")) {
   this.net <- net_euro_2004(paste0("data/",i,".dl.csv"))
   grecia.nets[[i]] = this.net
-  grecia <- rbind(grecia,data.frame(game=i,entropy=entropy(E(this.net)$weight)))
+  these.entropies <- entropies(E(this.net)$weight)   
+  grecia <- rbind(grecia,data.frame(game=i,
+                                    entropy=these.entropies[[1]],
+                                    normEntropy=these.entropies[[2]]))
+    
+  states <- length(unique(E(this.net)$weight))
   bd.ranking <- rbind(bd.ranking,
                       data.frame(player=V(this.net)$name,
                                  team="Greece",
@@ -50,4 +63,9 @@ for (i in 2:6) {
 ggplot(bd.ranking[ bd.ranking$game=="portugal" | bd.ranking$game=="grecia",], aes(x=entropy,y=betweenness,shape=team,color=game,label=player))+geom_point()+geom_text(hjust=1,vjust=-1)
 ggplot(bd.ranking[ bd.ranking$game=="portugal-6" | bd.ranking$game=="grecia-6",], aes(x=entropy,y=betweenness,shape=team,color=game,label=player))+geom_point()+geom_text(hjust=1,vjust=-1)
 
-plot(grecia.nets[[2]],edge.width = E(grecia.nets[[2]])$weight,vertex.size = V(grecia.nets[[2]])$betweenness, vertex.color="white", vertex.border.color="blue")
+adj_matrix <- as.matrix(read.csv("data/grecia-2.dl.csv",row.names=1, check.names=FALSE, na.strings = ""))
+net.2004 <- graph_from_adjacency_matrix(adj_matrix,mode="undirected")
+isolated <- which(degree(net.2004)==0)
+net.2004 <- delete.vertices(net.2004, isolated)
+greece.vs.spain.cluster <-  cluster_edge_betweenness(net.2004,directed=F)
+plot(greece.vs.spain.cluster,net.2004)
