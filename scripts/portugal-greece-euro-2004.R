6f441b8e6ab304bb91a01c2a876e046fc240d0a7
JJ Merelo <jjmerelo@gmail.com>
2022-11-06 19:12:09 +0100

    Charting overall entropy

--- scripts/portugal-greece-euro-2004.R
+++ scripts/portugal-greece-euro-2004.R
@@ -1,7 +1,8 @@
 library(igraph)
-
+library(dplyr)
 load("data/euro-2004.RData")
 load("data/euro-2004-betw-flow-diversity.RData")
+load("data/euro-2004-player.RData")
 
 portugal <- euro2004.nets[["portugal-6"]]
 greece <- euro2004.nets[["grecia-6"]]
@@ -17,3 +18,7 @@ diversity.greece <- data.frame( rank=1:length(V(greece)$diversity),entropy=sort(
 ggplot()+geom_point(data=diversity.portugal,shape=21,stroke=2,color='darkgreen',fill='red',aes(x=rank,y=entropy,size=5))  +geom_point(data=diversity.greece,shape=21,stroke=2,fill='blue',color='white',aes(x=rank,y=entropy),size=5)
 
 ggplot(bd.ranking[ bd.ranking$game=="portugal-6" | bd.ranking$game=="grecia-6",], aes(x=entropy,y=flow,shape=team,color=game,label=player))+geom_point()+geom_text(hjust=1,vjust=-1)+theme_tufte()+xlim(0.65,1)
+
+
+euro2004$team <- as.factor(as.character(lapply( euro2004$game, function(x) strsplit(x,"-")[[1]][1])))
+ggplot(euro2004, aes(x=reorder(game,entropy),y=entropy,color=team))+geom_point()+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+xlab("Game")
