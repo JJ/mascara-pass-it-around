7c034c41fd43b49bca1c146d0f858a3e07e79622
JJ Merelo <jjmerelo@gmail.com>
2022-11-06 12:55:14 +0100

    Working with flow and diversity

--- scripts/france-greece-euro-2004.R
+++ scripts/france-greece-euro-2004.R
@@ -1,3 +1,11 @@
+library(igraph)
 
 load("data/euro-2004.RData")
 
+france <- euro2004.nets[["francia-4"]]
+greece <- euro2004.nets[["grecia-3"]]
+
+plot(france,edge.width = E(france)$weight,vertex.size = V(france)$flow, vertex.color="blue",vertex.frame.color="red")
+plot(greece,edge.width = E(greece)$weight,vertex.size = V(greece)$flow, vertex.color="white",vertex.frame.color="blue")
+print(mean(V(france)$flow))
+print(mean(V(france)$flow))
