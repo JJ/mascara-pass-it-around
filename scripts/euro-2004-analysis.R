490f29c408abaef80f06de5700949bc6ffd17138
JJ Merelo <jjmerelo@gmail.com>
2022-11-08 14:59:05 +0100

    Adapts chart

--- scripts/euro-2004-analysis.R
+++ scripts/euro-2004-analysis.R
@@ -7,7 +7,7 @@ load(paste0(PREFIX,"euro-2004-betw-flow-diversity.RData"))
 load(paste0(PREFIX,"euro-2004-player.RData"))
 
 euro2004$team <- as.factor(as.character(lapply( euro2004$game, function(x) strsplit(x,"-")[[1]][1])))
-ggplot(euro2004, aes(x=reorder(game,entropy),y=entropy,color=team))+geom_point()+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+xlab("Game")
+ggplot(euro2004, aes(x=reorder(game,tsallisEntropy),y=tsallisEntropy,color=team))+geom_point()+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+xlab("Game")
 
 matches <- read.csv(paste0(PREFIX,"partidos.csv"))
 
@@ -15,8 +15,8 @@ entropy.diffs <- data.frame(match=character(),scorediff=numeric(),entropydiff=nu
 for (match in 1:nrow(matches)) {
   team1 <- matches[match,"Team1"]
   team2 <- matches[match,"Team2"]
-  entropy1 <- unique(euro2004[euro2004$game==team1,]$entropy)
-  entropy2 <- unique(euro2004[euro2004$game==team2,]$entropy)
+  entropy1 <- unique(euro2004[euro2004$game==team1,]$tsallisEntropy)
+  entropy2 <- unique(euro2004[euro2004$game==team2,]$tsallisEntropy)
   entropy.diffs <- rbind(entropy.diffs,
                          data.frame(match=paste0(team1,"-",team2),
                                     scorediff=matches[match,"Goals1"]-matches[match,"Goals2"],
@@ -25,3 +25,4 @@ for (match in 1:nrow(matches)) {
 }
 
 ggplot(entropy.diffs,aes(x=entropydiff,y=scorediff))+geom_point()
+
