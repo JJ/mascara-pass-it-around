920b63b9c2e3146ccea748e1fcfd13edfd5e9ae4
JJ Merelo <jjmerelo@gmail.com>
2022-11-08 10:11:28 +0100

    Cleans up rest of files

--- scripts/save-rds.R
+++ scripts/save-rds.R
@@ -1,24 +1,16 @@
-library(tidyverse)
-library(dplyr)
-library(igraph)
-library(entropy)
-
+PREFIX <- "data/euro-2004/"
 source("scripts/net_euro_2004.R")
 
+matches <- read.csv(paste0(PREFIX,"partidos.csv"))
+                    
 euro2004 <- data.frame(game=character(),entropy=numeric(),normEntropy=numeric())
 euro2004.nets <- list()
 bd.ranking <- data.frame(player = character(), team = character(), game = character(), bd = numeric(), entropy = numeric(), betweenness = numeric(),flow=numeric())
-for (i in c(
-           "portugal","portugal-2","portugal-3", "portugal-4", "portugal-5", "portugal-6",
-          "grecia","grecia-2","grecia-3", 
-           "grecia-5", "grecia-6",
-            "francia", "francia-2", "francia-4",
-            "spain", "spain-2")) {
-  this.net <- net_euro_2004(paste0("data/",i,".dl.csv"))
+for (i in c(matches$Team1,matches$Team2)) {
+  this.net <- net_euro_2004(paste0(PREFIX,i,".dl.csv"))
   euro2004.nets[[i]] = this.net
   euro2004 <- rbind(euro2004,data.frame(game=i,entropy=entropy(E(this.net)$weight)))
   team <- strsplit(i, "-")[[1]][1]
-  print(team)
   bd.ranking <- rbind(bd.ranking,
                       data.frame(player=V(this.net)$name,
                                  team=team,
